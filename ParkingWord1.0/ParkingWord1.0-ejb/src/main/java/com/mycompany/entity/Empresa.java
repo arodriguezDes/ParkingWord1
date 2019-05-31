0ce31d7f76089a1a42a8ba71dcddbfd834c938c1
arodriguezDes <andres-rodriguez83@hotmail.com>
2019-05-31 17:48:33 -0500

    Agregando mapeo relacional en empresa entity modulo EJB

--- ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Empresa.java
+++ ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Empresa.java
@@ -1,9 +1,115 @@
 package com.mycompany.entity;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 /**
  *
  * @author Andres
  */
-public class Empresa {
+@Entity
+@Table(name = "empresa")
+@XmlRootElement
+
+public class Empresa implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_empresa")
+    private Integer idEmpresa;
+    @Size(max = 50)
+    @Column(name = "razon_social")
+    private String razonSocial;
+    @Size(max = 50)
+    @Column(name = "nit")
+    private String nit;
+    @Size(max = 100)
+    @Column(name = "direccion")
+    private String direccion;
+    @Column(name = "telefono")
+    private Integer telefono;
+    @Column(name = "numero_cupos")
+    private Integer numeroCupos;
+    @OneToMany(mappedBy = "empresa")
+    private List<Cliente> clienteList;
+    
+    public Empresa(){
+        
+    }
+    
+    public Empresa(Integer idEmpresa){
+        this.idEmpresa = idEmpresa;
+    }
+
+    public Integer getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(Integer idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public Integer getNumeroCupos() {
+        return numeroCupos;
+    }
+
+    public void setNumeroCupos(Integer numeroCupos) {
+        this.numeroCupos = numeroCupos;
+    }
+    
+    @XmlTransient
+    public List<Cliente> getClienteList() {
+        return clienteList;
+    }
+
+    public void setClienteList(List<Cliente> clienteList) {
+        this.clienteList = clienteList;
+    }
     
 }
