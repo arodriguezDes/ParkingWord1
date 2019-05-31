37be257336001962879b8d463a2f1eab72ecdbc4
arodriguezDes <andres-rodriguez83@hotmail.com>
2019-05-31 16:05:48 -0500

    Agregando mapeo relacional en parqueadero entity modulo EJB

--- ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Parqueadero.java
+++ ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Parqueadero.java
@@ -1,9 +1,113 @@
 package com.mycompany.entity;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 /**
  *
  * @author Andres
  */
-public class Parqueadero {
+@Entity
+@Table(name = "parqueadero")
+@XmlRootElement
+
+public class Parqueadero implements Serializable{
     
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_parqueadero")
+    private Integer idParqueadero;
+    @Size(max = 50)
+    @Column(name = "nombre")
+    private String nombre;
+    @Size(max = 50)
+    @Column(name = "direccion")
+    private String direccion;
+    @Size(max = 20)
+    @Column(name = "telefono")
+    private String telefono;
+    @Column(name = "numero_cupos")
+    private Integer numeroCupos;
+    @Size(max = 10)
+    @Column(name = "estado")
+    private String estado;
+    @OneToMany(mappedBy = "parqueadero")
+    private List<Vehiculo> vehiculoList;
+
+    public Parqueadero() {
+    }
+
+    public Parqueadero(Integer idParqueadero) {
+        this.idParqueadero = idParqueadero;
+    }
+
+    public Integer getIdParqueadero() {
+        return idParqueadero;
+    }
+
+    public void setIdParqueadero(Integer idParqueadero) {
+        this.idParqueadero = idParqueadero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Integer getNumeroCupos() {
+        return numeroCupos;
+    }
+
+    public void setNumeroCupos(Integer numeroCupos) {
+        this.numeroCupos = numeroCupos;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @XmlTransient
+    public List<Vehiculo> getVehiculoList() {
+        return vehiculoList;
+    }
+
+    public void setVehiculoList(List<Vehiculo> vehiculoList) {
+        this.vehiculoList = vehiculoList;
+    }
 }
