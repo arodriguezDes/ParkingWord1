eadb0ae3b2ce302044a498e6f5df7426152e0abe
arodriguezDes <andres-rodriguez83@hotmail.com>
2019-05-31 18:27:48 -0500

    Agregando mapeo relacional en contrato y su selector TipoContrato en modulo EJB

--- ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Contrato.java
+++ ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Contrato.java
@@ -1,9 +1,125 @@
 package com.mycompany.entity;
 
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * @author Andres
  */
-public class Contrato {
+@Entity
+@Table(name = "contrato")
+@XmlRootElement
+
+public class Contrato implements Serializable{
     
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_contrato")
+    private Integer idContrato;
+    @Size(max = 10)
+    @Column(name = "placa")
+    private String placa;
+    @Size(max = 20)
+    @Column(name = "fecha_inicio")
+    private String fechaInicio;
+    @Size(max = 20)
+    @Column(name = "fecha_fin")
+    private String fechaFin;
+    @Column(name = "numero_dias")
+    private Integer numeroDias;
+    @Size(max = 10)
+    @Column(name = "estado")
+    private String estado;
+    @JoinColumn(name = "id_tipo_contrato", referencedColumnName = "id_tipo_contrato")
+    @ManyToOne
+    private TipoContrato tipoContrato;
+    @JoinColumn(name = "id_persona", referencedColumnName = "id_persona")
+    @ManyToOne
+    private Persona persona;
+    
+    public Contrato() {
+    }
+    
+    public Contrato(Integer idContrato ) {
+        this.idContrato = idContrato;
+    }
+
+    public Integer getIdContrato() {
+        return idContrato;
+    }
+
+    public void setIdContrato(Integer idContrato) {
+        this.idContrato = idContrato;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Integer getNumeroDias() {
+        return numeroDias;
+    }
+
+    public void setNumeroDias(Integer numeroDias) {
+        this.numeroDias = numeroDias;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public TipoContrato getTipoContrato() {
+        return tipoContrato;
+    }
+
+    public void setTipoContrato(TipoContrato tipoContrato) {
+        this.tipoContrato = tipoContrato;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }    
+      
 }
--- ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/TipoContrato.java
+++ ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/TipoContrato.java
@@ -1,9 +1,84 @@
 package com.mycompany.entity;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 /**
  *
  * @author Andres
  */
-public class TipoContrato {
+@Entity
+@Table(name = "tipo_contrato")
+@XmlRootElement
+
+public class TipoContrato implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_tipo_contrato")
+    private Integer idTipoContrato;
+    @Size(max = 50)
+    @Column(name = "tipo_contrato")
+    private String tipoContrato;
+    @Size(max = 10)
+    @Column(name = "estado")
+    private String estado;
+    @OneToMany(mappedBy = "tipoContrato")
+    private List<Contrato> contratoList;
+    
+    public TipoContrato(){
+        
+    }
+    
+    public TipoContrato(Integer idTipoContrato){
+        this.idTipoContrato = idTipoContrato;
+    }
+
+    public Integer getIdTipoContrato() {
+        return idTipoContrato;
+    }
+
+    public void setIdTipoContrato(Integer idTipoContrato) {
+        this.idTipoContrato = idTipoContrato;
+    }
+
+    public String getTipoContrato() {
+        return tipoContrato;
+    }
+
+    public void setTipoContrato(String tipoContrato) {
+        this.tipoContrato = tipoContrato;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @XmlTransient
+    public List<Contrato> getContratoList() {
+        return contratoList;
+    }
+
+    public void setContratoList(List<Contrato> contratoList) {
+        this.contratoList = contratoList;
+    }
     
 }
