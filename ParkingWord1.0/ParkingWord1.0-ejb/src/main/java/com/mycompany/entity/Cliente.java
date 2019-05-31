71d2d86cfce2769423421d48d15360bf1d16781a
arodriguezDes <andres-rodriguez83@hotmail.com>
2019-05-31 17:25:15 -0500

    Agregando mapeo relacional de cliente entity en modulo EJB

--- ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Cliente.java
+++ ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Cliente.java
@@ -1,9 +1,119 @@
 package com.mycompany.entity;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 /**
  *
  * @author Andres
  */
-public class Cliente {
+@Entity
+@Table(name = "cliente")
+@XmlRootElement
+
+public class Cliente implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_persona")
+    private Integer idPersona;
+    @Size(max = 100)
+    @Column(name = "codigo_cliente")
+    private String codigoCliente;
+    @Size(max = 45)
+    @Column(name = "tipo_cliente")
+    private String tipoCliente;
+    @JoinColumn(name = "id_persona", referencedColumnName = "id_persona", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private Persona persona;
+    @OneToMany(mappedBy = "cliente")
+    private List<Reserva> reservaList;
+    @OneToMany(mappedBy = "persona")
+    private List<Contrato> contratoList;
+    @JoinColumn(name = "id_empresa", referencedColumnName = "id_empresa")
+    @ManyToOne
+    private Empresa empresa;
+    
+    public Cliente(){
+        
+    }
+    
+    public Cliente(Integer idPersona){
+        this.idPersona = idPersona;
+    }
+
+    public Integer getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Integer idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(String codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    @XmlTransient
+    public List<Reserva> getReservaList() {
+        return reservaList;
+    }
+
+    public void setReservaList(List<Reserva> reservaList) {
+        this.reservaList = reservaList;
+    }
+
+    @XmlTransient
+    public List<Contrato> getContratoList() {
+        return contratoList;
+    }
+
+    public void setContratoList(List<Contrato> contratoList) {
+        this.contratoList = contratoList;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
     
 }
