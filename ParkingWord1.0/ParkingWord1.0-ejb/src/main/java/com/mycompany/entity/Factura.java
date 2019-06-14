7078c21dc9ac3783b63fbaff64e0204c46fd2579
arodriguezDes <andres-rodriguez83@hotmail.com>
2019-06-13 21:43:58 -0500

    Agregando mapeo relacional de entity factura en modulo EJB

--- ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Factura.java
+++ ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Factura.java
@@ -1,9 +1,92 @@
 package com.mycompany.entity;
 
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * @author Andres
  */
-public class Factura {
+@Entity
+@Table(name = "factura")
+@XmlRootElement
+
+public class Factura implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_factura")
+    private Integer idFactura;
+    @Size(max = 255)
+    @Column(name = "estado")
+    private String estado;
+    @Column(name = "numero_factura")
+    private Integer numeroFactura;
+    @JoinColumn(name = "id_pago", referencedColumnName = "id_pago")
+    @ManyToOne
+    private Pago pago;
+    @JoinColumn(name = "id_tipo_comprobante", referencedColumnName = "id_tipo_comprobante")
+    @ManyToOne
+    private TipoComprobante tipoComprobante;
+
+    public Factura() {
+    }
+
+    public Factura(Integer idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public Integer getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(Integer idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Integer getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(Integer numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public Pago getPago() {
+        return pago;
+    }
+
+    public void setPago(Pago pago) {
+        this.pago = pago;
+    }
+
+    public TipoComprobante getTipoComprobante() {
+        return tipoComprobante;
+    }
+
+    public void setTipoComprobante(TipoComprobante tipoComprobante) {
+        this.tipoComprobante = tipoComprobante;
+    }
     
 }
