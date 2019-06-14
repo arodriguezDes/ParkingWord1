2afd74fd9f475fda30f477b734a0d9747db51325
arodriguezDes <andres-rodriguez83@hotmail.com>
2019-06-13 22:24:32 -0500

    Agregando mapeo relacional de entity tipoComprobante en modulo EJB

--- ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/TipoComprobante.java
+++ ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/TipoComprobante.java
@@ -1,9 +1,82 @@
 package com.mycompany.entity;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * @author Andres
  */
-public class TipoComprobante {
+@Entity
+@Table(name = "tipo_comprobante")
+@XmlRootElement
+
+public class TipoComprobante implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_tipo_comprobante")
+    private Integer idTipoComprobante;
+    @Size(max = 255)
+    @Column(name = "estado")
+    private String estado;
+    @Size(max = 255)
+    @Column(name = "tipo_comprobante")
+    private String tipoComprobante;
+    @OneToMany(mappedBy = "tipoComprobante")
+    private List<Factura> facturaList;
+
+    public TipoComprobante() {
+    }
+
+    public TipoComprobante(Integer idTipoComprobante) {
+        this.idTipoComprobante = idTipoComprobante;
+    }
+
+    public Integer getIdTipoComprobante() {
+        return idTipoComprobante;
+    }
+
+    public void setIdTipoComprobante(Integer idTipoComprobante) {
+        this.idTipoComprobante = idTipoComprobante;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTipoComprobante() {
+        return tipoComprobante;
+    }
+
+    public void setTipoComprobante(String tipoComprobante) {
+        this.tipoComprobante = tipoComprobante;
+    }
+
+    public List<Factura> getFacturaList() {
+        return facturaList;
+    }
+
+    public void setFacturaList(List<Factura> facturaList) {
+        this.facturaList = facturaList;
+    }
+    
     
 }
