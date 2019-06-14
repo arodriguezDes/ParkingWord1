c9bc01f77a790f7f3273f8e37841e3752ffa8eaa
arodriguezDes <andres-rodriguez83@hotmail.com>
2019-06-13 21:31:13 -0500

    Agregando mapeo relacional en entity convenio en modulo EJB

--- ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Convenio.java
+++ ParkingWord1.0/ParkingWord1.0-ejb/src/main/java/com/mycompany/entity/Convenio.java
@@ -1,9 +1,104 @@
 package com.mycompany.entity;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 /**
  *
  * @author Andres
  */
-public class Convenio {
+@Entity
+@Table(name = "convenio")
+@XmlRootElement
+
+public class Convenio implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_convenio")
+    private Integer idConvenio;
+    @Size(max = 45)
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "descuento")
+    private Integer descuento;
+    @Size(max = 45)
+    @Column(name = "empresa")
+    private String empresa;
+    @Size(max = 15)
+    @Column(name = "estado")
+    private String estado;
+    @OneToMany(mappedBy = "convenio")
+    private List<Pago> pagoList;
+
+    public Convenio() {
+    }
+
+    public Convenio(Integer idConvenio) {
+        this.idConvenio = idConvenio;
+    }
+
+    public Integer getIdConvenio() {
+        return idConvenio;
+    }
+
+    public void setIdConvenio(Integer idConvenio) {
+        this.idConvenio = idConvenio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(Integer descuento) {
+        this.descuento = descuento;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    @XmlTransient
+    public List<Pago> getPagoList() {
+        return pagoList;
+    }
+
+    public void setPagoList(List<Pago> pagoList) {
+        this.pagoList = pagoList;
+    }
     
 }
